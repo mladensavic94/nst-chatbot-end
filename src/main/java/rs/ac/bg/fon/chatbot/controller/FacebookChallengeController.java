f47a6c6d8ae1dae9df346cf856ea65f8278f6e93
Mladen Savic <mladensavic94@gmail.com>
2017-12-25 22:14:43 +0100

    Token verification

--- src/main/java/rs/ac/bg/fon/chatbot/controller/FacebookChallengeController.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/FacebookChallengeController.java
@@ -8,6 +8,7 @@ package rs.ac.bg.fon.chatbot.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,11 +19,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/rest")
 public class FacebookChallengeController {
-    
-    private final String TOKEN = "Jedi govna majmune";
-    
+
+    private final String TOKEN = "EAAZATKY8ZBibMBAJzzyx384nZBWRPAnIJy6u7RvEIFT36rmZBawFv8R5QjZB54V2FJ8suhzouRYtoDJyWtsvc1DgC4lZCFXkxY2o2XHHsVN2iiMrfrWafQjGaQ0VPnREZBH6cU5NxnKsPyCWqJJMq0y5iw6HHAHmaapZBHGryU3dgYjQfLAnONbv";
+
     @RequestMapping("/validate")
-    public @ResponseBody Object validateFacebookApp(){
-        return ResponseEntity.status(HttpStatus.OK).body(TOKEN);
+    public @ResponseBody Object validateFacebookApp(@PathVariable String mode, @PathVariable String verify_token,
+                                                    @PathVariable String challenge){
+        if(verify_token.equals(TOKEN))
+        return ResponseEntity.status(HttpStatus.OK).body(challenge);
+        return null;
     }
 }
