23e0825bac5bf0833a8cd323b981d1b083578c0b
Mladen Savic <mladensavic94@gmail.com>
2017-12-25 23:56:09 +0100

    Logging service added

--- src/main/java/rs/ac/bg/fon/chatbot/SpringChatbotApplication.java
+++ src/main/java/rs/ac/bg/fon/chatbot/SpringChatbotApplication.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "rs.ac.bg.fon.chatbot")
-@EnableJpaRepositories("rs.ac.bg.fon.chatbot.db")
 public class SpringChatbotApplication {
 
 	public static void main(String[] args) {
--- src/main/java/rs/ac/bg/fon/chatbot/db/LogsService.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/LogsService.java
@@ -1,9 +1,7 @@
 package rs.ac.bg.fon.chatbot.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface LogsService extends JpaRepository<Logs, String> {
 
 }
