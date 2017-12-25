41bf772cee63d9913c626e462446747e722947d1
Mladen Savic <mladensavic94@gmail.com>
2017-12-25 22:41:53 +0100

    Token verification

--- src/main/java/rs/ac/bg/fon/chatbot/config/SpringSecurityConfig.java
+++ src/main/java/rs/ac/bg/fon/chatbot/config/SpringSecurityConfig.java
@@ -21,6 +21,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
+        http.csrf().disable();
     }
     
    
