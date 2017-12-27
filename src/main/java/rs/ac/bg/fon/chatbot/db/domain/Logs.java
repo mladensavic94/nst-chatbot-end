f4b17ca0f0c956eb41ffbddafe9acaa3ab5d2357
Mladen Savic <mladensavic94@gmail.com>
2017-12-27 23:15:20 +0100

    Entity manager factory added

--- src/main/java/rs/ac/bg/fon/chatbot/db/domain/Logs.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/domain/Logs.java
@@ -1,17 +1,38 @@
 package rs.ac.bg.fon.chatbot.db.domain;
 
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Logs{
 
     @Id
-    String log;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String log;
 
     public Logs(String log){
+        this.log = log;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLog() {
+        return log;
+    }
+
+    public void setLog(String log) {
         this.log = log;
     }
 }
