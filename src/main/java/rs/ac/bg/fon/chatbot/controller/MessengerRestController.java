bd8b6725d7a364810807ebb123a89846d888dda9
Mladen Savic <mladensavic94@gmail.com>
2017-12-28 22:47:22 +0100

    added parsing try 4

--- src/main/java/rs/ac/bg/fon/chatbot/controller/MessengerRestController.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/MessengerRestController.java
@@ -34,7 +34,7 @@ public class MessengerRestController {
     @RequestMapping(value = "/chatbot", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> receiveMessage(@RequestBody String json){
         try {
-            json = "{" + json.substring(17);
+            json =json.substring(25, json.length()-1);
             Iterator<TextMessageEvent> events = genson.deserializeValues(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8.name())), TextMessageEvent.class);
             while(events.hasNext()){
                 System.out.println("LOGGING: "   + events.next().senderId());
