ff18ca7b31734235d73815313c932ada02702878
Mladen Savic <mladensavic94@gmail.com>
2018-01-16 23:51:11 +0100

    rest api for professor and office hours

--- src/main/java/rs/ac/bg/fon/chatbot/db/repositories/OfficeHoursRepository.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/repositories/OfficeHoursRepository.java
@@ -2,11 +2,11 @@ package rs.ac.bg.fon.chatbot.db.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import rs.ac.bg.fon.chatbot.db.domain.OfficeHours;
 
 public interface OfficeHoursRepository extends CrudRepository<OfficeHours, Integer> {
 
-    @Query("select o from OfficeHours o where o.idprofessor = :id")
-    Iterable<OfficeHours> findAllByProfessorId(@Param("id") Long id);
+    @Query("select o from OfficeHours o where o.professor.id = ?1")
+    Iterable<OfficeHours> findAllByProfessorId(Long id);
+
 }
