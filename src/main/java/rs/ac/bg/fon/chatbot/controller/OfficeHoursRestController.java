16e7db6c5c6c95891482431708b774093f1ddbe9
Mladen Savic <mladensavic94@gmail.com>
2018-01-16 23:56:17 +0100

    rest api for professor and office hours

--- src/main/java/rs/ac/bg/fon/chatbot/controller/OfficeHoursRestController.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/OfficeHoursRestController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import rs.ac.bg.fon.chatbot.ParsingUtil;
 import rs.ac.bg.fon.chatbot.db.services.OfficeHoursService;
 
@@ -21,7 +22,7 @@ public class OfficeHoursRestController {
 
 
     @RequestMapping(value = "/officehours", method = RequestMethod.GET)
-    public ResponseEntity<Object> getOfficeHoursForProfessor(@RequestBody Long id){
+    public ResponseEntity<Object> getOfficeHoursForProfessor(@RequestParam Long id){
         try {
             String response = ParsingUtil.parseListToJson(officeHoursService.findAllByProfessorId(Math.toIntExact(id)));
             System.out.println(response);
