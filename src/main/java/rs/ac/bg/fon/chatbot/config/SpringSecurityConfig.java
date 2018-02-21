7d39f992295cafd90027eb2fd007aba324b50be0
Mladen Savic <mladensavic94@gmail.com>
2018-02-21 19:04:17 +0100

    cors configuration

--- src/main/java/rs/ac/bg/fon/chatbot/config/SpringSecurityConfig.java
+++ src/main/java/rs/ac/bg/fon/chatbot/config/SpringSecurityConfig.java
@@ -35,7 +35,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("*", "http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/*", configuration);
