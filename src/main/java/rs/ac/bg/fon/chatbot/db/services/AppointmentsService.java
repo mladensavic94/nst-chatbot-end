7550695d00ff2e589f7163355bd9c3576dd569e0
Mladen Savic <mladensavic94@gmail.com>
2018-03-05 20:51:24 +0100

    appointment rest controller added

--- src/main/java/rs/ac/bg/fon/chatbot/db/services/AppointmentsService.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/services/AppointmentsService.java
@@ -3,19 +3,25 @@ package rs.ac.bg.fon.chatbot.db.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.ac.bg.fon.chatbot.db.domain.Appointment;
+import rs.ac.bg.fon.chatbot.db.domain.OfficeHours;
 import rs.ac.bg.fon.chatbot.db.repositories.AppointmentsRepository;
+import rs.ac.bg.fon.chatbot.db.repositories.OfficeHoursRepository;
 
 @Service("appointmentService")
 public class AppointmentsService {
 
     @Autowired
     AppointmentsRepository appointmentsRepository;
+    @Autowired
+    OfficeHoursRepository officeHoursRepository;
 
     public Iterable<Appointment> findAll() {
         return appointmentsRepository.findAll();
     }
 
     public void save(Appointment appointment){
+        OfficeHours officeHours = officeHoursRepository.findOne(appointment.getOfficeHours().getId());
+        appointment.setOfficeHours(officeHours);
         appointmentsRepository.save(appointment);
     }
 }
