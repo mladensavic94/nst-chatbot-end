4895468ffb3a9943e7d98929dbfd4644c41be4a1
Mladen Savic <mladensavic94@gmail.com>
2018-03-07 22:21:12 +0100

    change in responce logic

--- src/main/java/rs/ac/bg/fon/chatbot/controller/ResponseService.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/ResponseService.java
@@ -4,7 +4,6 @@ import com.eclipsesource.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import rs.ac.bg.fon.chatbot.CommunicationLevelHolder;
@@ -12,19 +11,16 @@ import rs.ac.bg.fon.chatbot.db.domain.Appointment;
 import rs.ac.bg.fon.chatbot.db.domain.Level;
 import rs.ac.bg.fon.chatbot.db.domain.Message;
 
-import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
-
-import static rs.ac.bg.fon.chatbot.db.domain.Level.DATUM;
 
 @Service
 public class ResponseService {
 
     private final static String TOKEN = "EAAZATKY8ZBibMBAEsVy3ZA4F73jSboFAfukwl9Qa66VfFtXiAR7TTYRcSLjBjryTBZAu88j3ZAocIXyX2VdXe2EgVVUw0BdUXsiXdWEKodcr1Dh11LrUDNrTi2aTW3FKLCbVHtSRgRRR6uQJ3Jd4C47RvIibFS7wLu6xTFW6c2e9FQQ0qSsjE";
-    private final static String URL = "https://graph.facebook.com/v2.6/me/messages?access_token=" + TOKEN;
-    private Message message;
+    private final static String URL_FACEBOOK = "https://graph.facebook.com/v2.6/me/messages?access_token=" + TOKEN;
+    private final static String WIT_AI_VERSION = "20180307";
+    private final static String URL_WIT_AI = "https://api.wit.ai/message?v=" + WIT_AI_VERSION + "&q=";
     private static Map<String, Appointment> appointmentCollection;
 
     static {
@@ -48,7 +44,7 @@ public class ResponseService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(response, headers);
-        sendAnswer.postForLocation(URL, entity);
+        sendAnswer.postForLocation(URL_FACEBOOK, entity);
     }
 
     private String generateAnswer(Message message) {
@@ -75,7 +71,7 @@ public class ResponseService {
 
     public void add(Message message) {
         Appointment appointment = appointmentCollection.get(message.getSenderID());
-        if(appointment == null){
+        if (appointment == null) {
             appointment = new Appointment();
         }
         parseAppointmentFromMessage(message.getText());
@@ -86,10 +82,10 @@ public class ResponseService {
     private String parseAppointmentFromMessage(Object text) {
         RestTemplate getUserInfo = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.add("Authorization", "Bearer D5Y74IMSC5YWUFNJUC477S5GLTXGWDSG");
-        HttpEntity<?> requestEntity = new HttpEntity<>(text, headers);
-        ResponseEntity<String> json = getUserInfo.exchange(URL, HttpMethod.GET, requestEntity, String.class);
+        headers.add("Content-type", "application/json");
+        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> json = getUserInfo.exchange(URL_WIT_AI+text, HttpMethod.GET, requestEntity, String.class);
         return json.getBody();
     }
 }
