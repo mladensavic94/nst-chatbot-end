c53044b6c25abb6a99ae94705619dcfc8e848b22
Mladen Savic <mladensavic94@gmail.com>
2018-04-03 21:59:10 +0200

    a

--- src/main/java/rs/ac/bg/fon/chatbot/controller/ResponseService.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/ResponseService.java
@@ -98,6 +98,7 @@ public class ResponseService {
                 response = "Kod kog profesora zelite na konsultacije?";
             }
             try {
+                System.out.println(appointment.getProfessor());
                 OfficeHours officeHours = officeHoursService.filterByDate(parseDate(appointmentString), professor);
                 if (officeHours == null)
                     response = "U tom terminu nema konsultacija";
