3c6ce7d80d74398fbc03d64d5a3665aab8755d57
Mladen Savic <mladensavic94@gmail.com>
2018-04-11 22:49:13 +0200

    a

--- src/main/java/rs/ac/bg/fon/chatbot/controller/AppointmentRestController.java
+++ src/main/java/rs/ac/bg/fon/chatbot/controller/AppointmentRestController.java
@@ -31,19 +31,19 @@ public class AppointmentRestController {
     }
 
     @RequestMapping(value = "/appointments", method = RequestMethod.GET)
-    public ResponseEntity<Object> getAllAppointments(@RequestParam String email){
-        try{
+    public ResponseEntity<Object> getAllAppointments(@RequestParam String email) {
+        try {
             Iterable<Appointment> serviceAllByEmail = appointmentsService.findAllByEmail(email);
             String response = ParsingUtil.parseListToJson(serviceAllByEmail);
             return ResponseEntity.status(HttpStatus.OK).body(response);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     @RequestMapping(value = "/appointments/save", method = RequestMethod.POST)
-    public ResponseEntity<Object> saveAppointment(@RequestBody String json){
+    public ResponseEntity<Object> saveAppointment(@RequestBody String json) {
         try {
             Appointment appointment = ParsingUtil.parseJsonToDomainObject(json, Appointment.class);
             appointmentsService.save(appointment);
@@ -55,7 +55,7 @@ public class AppointmentRestController {
     }
 
     @RequestMapping(value = "/appointments/update", method = RequestMethod.POST)
-    public ResponseEntity<Object> updateAppointment(@RequestBody String json){
+    public ResponseEntity<Object> updateAppointment(@RequestBody String json) {
         try {
             System.out.println(json);
             Appointment appointment = ParsingUtil.parseJsonToDomainObject(json, Appointment.class);
@@ -63,11 +63,14 @@ public class AppointmentRestController {
             appointment = appointmentsService.findById(appointment.getId());
             System.out.println(appointment);
             String message;
-            if(status.equals(Status.ACCEPTED))
+            if (status.equals(Status.ACCEPTED)) {
                 message = "Vas zahtev za konsultacije je prihvacen";
-            else message = "Vas zahtev za konsultacije je odbijen";
+                appointment.setStatus(Status.ACCEPTED);
+            } else {
+                message = "Vas zahtev za konsultacije je odbijen";
+                appointment.setStatus(Status.DENIED);
+            }
             responseService.sendResponse(appointment.getStudentID(), message);
-            appointment.setStatus(status);
             appointmentsService.save(appointment);
             return ResponseEntity.status(HttpStatus.OK).body(appointment);
         } catch (Exception e) {
