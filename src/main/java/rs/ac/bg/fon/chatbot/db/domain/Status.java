97a75baf2883c5369e69a6deae55ff1bfbfe0bde
Mladen Savic <mladensavic94@gmail.com>
2018-04-11 22:32:36 +0200

    A

--- src/main/java/rs/ac/bg/fon/chatbot/db/domain/Status.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/domain/Status.java
@@ -1,8 +1,15 @@
 package rs.ac.bg.fon.chatbot.db.domain;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum Status {
 
+    @SerializedName("OPEN")
     OPEN,
+    @SerializedName("ACCEPTED")
     ACCEPTED,
-    FULL, DENIED
+    @SerializedName("FULL")
+    FULL,
+    @SerializedName("DENIED")
+    DENIED
 }
