754ec6c0e19700a8777ed25719749ae564cd5563
Mladen Savic <mladensavic94@gmail.com>
2018-04-15 17:09:50 +0200

    a

--- src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/AppointmentsRepository.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/AppointmentsRepository.java
@@ -6,7 +6,7 @@ import rs.ac.bg.fon.chatbot.db.domain.Appointment;
 
 public interface AppointmentsRepository extends CrudRepository<Appointment, Long> {
 
-    @Query("select a from Appointment a where a.officeHours.professor.email = ?1 order by a.officeHours.beginTime desc")
+    @Query("select a from Appointment a where a.professor.email = ?1 order by a.dateAndTime desc")
     Iterable<Appointment> findAllByEmail(String email);
 
     @Query("select a from Appointment a where a.studentID = ?1 and a.status = 'OPEN'")
