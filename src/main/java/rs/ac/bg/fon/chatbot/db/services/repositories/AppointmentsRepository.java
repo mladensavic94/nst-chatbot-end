03cdc887f189663311e3fbe1f39ce5f0a689db28
Mladen Savic <mladensavic94@gmail.com>
2018-05-28 21:56:06 +0200

    add tranactional

--- src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/AppointmentsRepository.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/AppointmentsRepository.java
@@ -2,6 +2,7 @@ package rs.ac.bg.fon.chatbot.db.services.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 import rs.ac.bg.fon.chatbot.db.domain.Appointment;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public interface AppointmentsRepository extends CrudRepository<Appointment, Long
     @Query("select a from Appointment a where a.studentID = ?1 and a.status = 'OPEN'")
     Appointment findByStudentID(String studentid);
 
+    @Transactional
     @Query("delete from Appointment a where a.studentID = ?1 and a.status= 'OPEN'")
     void deleteByStudentID(String s);
 }
