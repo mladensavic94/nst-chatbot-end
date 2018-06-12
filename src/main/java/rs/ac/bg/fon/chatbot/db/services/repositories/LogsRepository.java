4288b0fc141971709b409b2a61afe43b7be4ecf3
mladensavic94 <mladensavic94@gmail.com>
2018-06-12 11:21:37 +0200

    logs table droped

--- src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/LogsRepository.java
+++
@@ -1,8 +0,0 @@
-package rs.ac.bg.fon.chatbot.db.services.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import rs.ac.bg.fon.chatbot.db.domain.Logs;
-
-
-public interface LogsRepository extends CrudRepository<Logs, String> {
-}
