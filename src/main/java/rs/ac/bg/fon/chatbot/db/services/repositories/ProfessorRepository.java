85fb9d5fd634a27157a6ddfd7b0d1419d9568863
mladensavic94 <mladensavic94@gmail.com>
2018-06-12 12:57:58 +0200

    query backup

--- src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/ProfessorRepository.java
+++ src/main/java/rs/ac/bg/fon/chatbot/db/services/repositories/ProfessorRepository.java
@@ -11,7 +11,6 @@ public interface ProfessorRepository extends CrudRepository<Professor, Long>{
     @Query("select p from Professor p where p.email=?1 and p.password=?2")
     Professor findByUsernameAndPassword(String email, String password);
 
-    @Query("select o from OfficeHours o, Professor p where o.professor = p and p.email = ?1 order by o.beginTime desc")
+    @Query("select p from Professor p where p.email=?1")
     Professor findByUsername(String s);
-
 }
